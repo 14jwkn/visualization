d6e463fefed02f341fa967ae6032832b111faabc
14jwkn <jwk.ng@mail.utoronto.ca>
2026-02-02 23:41:00 -0500

    Created files according to assignment_3.md.

--- 02_activities/assignments/vis2_assignment_3.R
+++ 02_activities/assignments/vis2_assignment_3.R
@@ -13,7 +13,7 @@ df = df[df[,'population_group']=='All Population',]
 # Extract the year only and group by it, then calculate mean and standard error
 # across months for that year.
 df_stats <- df %>%
-  mutate(year=str_sub(date,-2)) %>%
+  mutate(year=paste0('20',str_sub(date,-2))) %>%
   group_by(year) %>%
   summarize(
     avg_identified=mean(newly_identified),
@@ -23,14 +23,16 @@ df_stats <- df %>%
   mutate(se_identified=sd_identified/sqrt(n))
 
 # Plot newly identified homeless over time.
-p <- ggplot(df,aes(x=year,y=avg_identified)) +
-  geom_col(fill='steelblue') +
+p <- ggplot(df_stats,aes(x=year,y=avg_identified)) + # Plot mean.
+  geom_col(fill='grey') +
   geom_errorbar(aes(ymin=avg_identified-se_identified, 
                     ymax=avg_identified+se_identified), 
-                width=0.2,color='red') +
-  labs(title='Newly Identified Individuals Experiencing Homelessness in Toronto Over Time',
+                width=0.2,color='black') + # Plot error bars.
+  labs(title='Newly Identified Homeless Individuals in Toronto Over Time', # Label, including source of information.
        x='Year',
-       y='Number of Individuals') +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle=90,hjust = 1))
-
+       y='Number',
+       caption='Source: Toronto Shelter & Support Services – Toronto Shelter System Flow (Toronto Open Data)') +
+  theme_minimal() + # Set theme with guide lines.
+  theme(plot.title = element_text(face='bold'),
+        plot.caption = element_text(face='bold',margin=margin(t=15)))
+ggsave('vis2_assignment_3.png',p,width=8,height=6) # Save.
